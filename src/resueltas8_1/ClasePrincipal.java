38fdd9c6173958905b140d3a0332a7c0fc715471
aoche <aoche@Gnehcoa>
2024-01-30 12:08:07 +0100

    coomiit2

---
+++ src/resueltas8_1/ClasePrincipal.java
@@ -0,0 +1,23 @@
+package resueltas8_1;
+
+public class ClasePrincipal {
+
+    public static void main(String[] args) {
+        Hora hora1 = new Hora(1, 1);
+        System.out.println(hora1);
+
+        System.out.println("Se ha podido establecer la hora: " + hora1.setHora(2));
+        System.out.println(hora1);
+
+        System.out.println("Se ha podido establecer la hora: " + hora1.setHora(25));
+
+        hora1.incrementarMinuto();
+        System.out.println(hora1);
+
+        System.out.println("Se ha podido establecer la hora: " + hora1.setMinuto(2));
+        System.out.println(hora1);
+        System.out.println("Se ha podido establecer la hora: " + hora1.setMinuto(90));
+        
+    }
+
+}
--- src/resueltas8_1/Hora.java
+++ src/resueltas8_1/Hora.java
@@ -1,11 +1,54 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
 package resueltas8_1;
-
 
 public class Hora {
 
+    int hora;
+    int minuto;
+
+    public Hora(int hora, int minuto) {
+        if (hora >= 0 && hora <= 23 && minuto >= 0 && minuto <= 59) {
+            this.hora = hora;
+            this.minuto = minuto;
+        } else {
+            System.out.println("Hora o minuto invalido.");
+        }
+
+    }
+    
+    public void incrementarMinuto(){
+        minuto++;
+        if(minuto == 60){
+            minuto=1;
+            hora++;
+        }
+    }
+    
+    public boolean setMinuto(int valor) {
+        this.minuto = valor;
+        boolean posible;
+        if(minuto >= 0 && minuto <= 59){
+            posible = true;
+        }else{
+            posible = false;
+        }
+        return posible;
+    }
+    
+    public boolean setHora(int valor){
+        this.hora = valor;
+        boolean posible;
+        if(hora >= 0 && hora <= 23){
+            posible = true;
+        }else{
+            posible = false;
+        }
+        return posible;
+    }
+
+    @Override
+    public String toString() {
+        return "Hora{" + "hora=" + hora + ", minuto=" + minuto + '}';
+    }
+
+       
 }
